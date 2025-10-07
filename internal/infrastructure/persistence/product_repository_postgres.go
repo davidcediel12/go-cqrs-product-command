b5e66773ade5f4933b6c4cddb15fa911a287ddd3
David Cediel <esdago18@gmail.com>
2025-10-06 21:49:48 -0500

    Create a transaction when saving product and images

--- internal/infrastructure/persistence/product_repository_postgres.go
+++ internal/infrastructure/persistence/product_repository_postgres.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -20,8 +21,15 @@ func NewProductRepository(pool *pgxpool.Pool) repository.ProductRepository {
 	}
 }
 
-func (r *ProductRepositoryImpl) CreateProduct(createProductRequest *dto.CreateProductRequest,
-	context context.Context) (dto.ProductDto, error) {
+func (r *ProductRepositoryImpl) CreateProduct(ctx context.Context, createProductRequest *dto.CreateProductRequest) (dto.ProductDto, error) {
+
+	transaction, err := r.pool.Begin(ctx)
+
+	if err != nil {
+		return dto.ProductDto{}, err
+	}
+
+	defer transaction.Rollback(ctx)
 
 	query := `
 		INSERT INTO products(id, product_name, price, stock) values ($1, $2, $3, $4)
@@ -29,14 +37,14 @@ func (r *ProductRepositoryImpl) CreateProduct(createProductRequest *dto.CreatePr
 
 	productId := uuid.New()
 
-	_, err := r.pool.Exec(context, query, productId, createProductRequest.Name,
+	_, err = transaction.Exec(ctx, query, productId, createProductRequest.Name,
 		createProductRequest.Price, createProductRequest.Stock)
 
 	if err != nil {
 		return dto.ProductDto{}, fmt.Errorf("saving product failed: %w", err)
 	}
 
-	productImages, err := r.saveProductImages(context, createProductRequest.Images, productId)
+	productImages, err := r.saveProductImages(ctx, transaction, createProductRequest.Images, productId)
 
 	if err != nil {
 		return dto.ProductDto{}, fmt.Errorf("saving product failed: %w", err)
@@ -51,7 +59,7 @@ func (r *ProductRepositoryImpl) CreateProduct(createProductRequest *dto.CreatePr
 	}, nil
 }
 
-func (r *ProductRepositoryImpl) saveProductImages(context context.Context,
+func (r *ProductRepositoryImpl) saveProductImages(ctx context.Context, transaction pgx.Tx,
 	productImages []dto.CreateProductImage, productId uuid.UUID) ([]dto.ProductImageDto, error) {
 
 	var createdImages []dto.ProductImageDto
@@ -64,7 +72,7 @@ func (r *ProductRepositoryImpl) saveProductImages(context context.Context,
 
 		productImageId := uuid.New()
 
-		_, err := r.pool.Exec(context, queryProductImage, productImageId, productId,
+		_, err := transaction.Exec(ctx, queryProductImage, productImageId, productId,
 			image.Url, image.IsPrimary)
 
 		if err != nil {
