6477e424e2196d7e66b21373710f1ab843647960
David Cediel <esdago18@gmail.com>
2025-10-06 21:29:24 -0500

    Implement repository to save product and images into the db

--- internal/infrastructure/persistence/product_repository_postgres.go
+++ internal/infrastructure/persistence/product_repository_postgres.go
@@ -1,8 +1,10 @@
 package persistence
 
 import (
+	"context"
 	"cqrs/command/internal/domain/repository"
 	"cqrs/command/internal/infrastructure/dto"
+	"log"
 
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -18,18 +20,60 @@ func NewProductRepository(pool *pgxpool.Pool) repository.ProductRepository {
 	}
 }
 
-func (r *ProductRepositoryImpl) CreateProduct(createProductRequest *dto.CreateProductRequest) dto.ProductDto {
-	return dto.ProductDto{
-		Id:    uuid.New(),
-		Name:  "first product",
-		Price: 182,
-		Stock: 2092,
-		Images: []dto.ProductImageDto{
-			{
-				Id:        uuid.New().String(),
-				Url:       "my.url",
-				IsPrimary: true,
-			},
-		},
+func (r *ProductRepositoryImpl) CreateProduct(createProductRequest *dto.CreateProductRequest, context context.Context) dto.ProductDto {
+
+	query := `
+		INSERT INTO products(id, product_name, price, stock) values ($1, $2, $3, $4)
+	`
+
+	productId := uuid.New()
+
+	_, err := r.pool.Exec(context, query, productId, createProductRequest.Name,
+		createProductRequest.Price, createProductRequest.Stock)
+
+	if err != nil {
+		log.Fatal("Saving product failed:", err)
 	}
+
+	productImages := r.saveProductImages(context, createProductRequest.Images, productId)
+
+	return dto.ProductDto{
+		Id:     productId,
+		Name:   createProductRequest.Name,
+		Price:  createProductRequest.Price,
+		Stock:  createProductRequest.Stock,
+		Images: productImages,
+	}
+}
+
+func (r *ProductRepositoryImpl) saveProductImages(context context.Context,
+	productImages []dto.CreateProductImage, productId uuid.UUID) []dto.ProductImageDto {
+
+	var createdImages []dto.ProductImageDto
+
+	queryProductImage := `
+		INSERT INTO product_images(id, product_id, url, is_primary) values ($1, $2, $3, $4)
+	`
+
+	for _, image := range productImages {
+
+		productImageId := uuid.New()
+
+		_, err := r.pool.Exec(context, queryProductImage, productImageId, productId,
+			image.Url, image.IsPrimary)
+
+		if err != nil {
+			log.Fatal("Saving product image failed:", err)
+		}
+
+		createdImages = append(createdImages, dto.ProductImageDto{
+			Id:        productImageId.String(),
+			Url:       image.Url,
+			IsPrimary: image.IsPrimary,
+		})
+
+	}
+
+	return createdImages
+
 }
